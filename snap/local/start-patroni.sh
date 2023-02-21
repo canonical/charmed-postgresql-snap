30ceb75d77e9b93a97dca7b5e492122493eb9b18
Jarred Wilson <jarred.wilson12@gmail.com>
2023-02-21 15:21:35 -0500

    Add wrapper script

---
+++ snap/local/start-patroni.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/bin/patroni "$@"
+
