4033ca46b0b5e5a4920bf7bc70e39cb16fd5eb29
Jarred Wilson <jarred.wilson12@gmail.com>
2023-03-08 12:25:32 -0500

    Updated to patroni 3

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -2,5 +2,5 @@
 
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/patroni "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/patroni $SNAP_DATA/patroni/config.yaml "$@"
 
