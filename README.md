bee734adab5198ad099e3fd9abd58f190e3f6d71
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-03-31 12:37:22 -0400

    Add snap implementation (#1)
    * Add snap implementation
    
    * Add CI implementation

---
+++ README.md
@@ -0,0 +1,32 @@
+PostgreSQL Snap
+This repository contains the packaging metadata for creating a snap of PostgreSQL built from the official Ubuntu repositories.  For more information on snaps, visit [snapcraft.io](https://snapcraft.io/). 
+
+## Installing the Snap
+The snap can be installed directly from the Stap Store.  Follow the link below for more information.
+<br>
+
+[![Get it from the Snap Store](https://snapcraft.io/static/images/badges/en/snap-store-black.svg)](https://snapcraft.io/charmed-postgresql)
+
+## Building the Snap
+### Clone Repository
+```bash
+git clone git@github.com:canonical/charmed-postgresql-snap.git
+cd charmed-postgresql-snap
+```
+### Installing and Configuring Prerequisites
+```bash
+sudo snap install snapcraft
+sudo snap install lxd
+sudo lxd init --auto
+```
+### Packing and Installing the Snap
+```bash
+snapcraft pack
+sudo snap install ./charmed-postgresql*.charm --devmode
+```
+
+## License
+The PostgreSQL Snap is free software, distributed under the Apache
+Software License, version 2.0. See
+[LICENSE](https://github.com/canonical/charme-postgresql-snap/blob/14.0/edge/licenses/LICENSE-snap)
+for more information.
---
+++ tests/test_smoke.py
@@ -0,0 +1,77 @@
+import yaml
+import subprocess
+import time
+import pytest
+
+
+def test_install():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        subprocess.run(
+            f"sudo snap install ./{snapcraft['name']}_{snapcraft['version']}_amd64.snap --devmode".split(),
+            check=True,
+        )
+
+
+@pytest.mark.run(after="test_install")
+def test_all_apps():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        override = {
+            "pgbackrest": "version",
+        }
+
+        skip = [
+            "patroni-aws",
+            "patronictl",
+            "patroni-wale-restore",
+            "patroni-raft-controller",
+            "pg-buildext",
+            "pg-conftool",
+            "pg-createcluster",
+            "pg-dropcluster",
+            "pg-receivexlog",
+            "pg-upgradecluster",
+            "pg-backupcluster",
+            "pg-restorecluster",
+            "pg-virtualenv",
+            "pg-ctlcluster",
+            "pg-renamecluster",
+            "pg-updatedicts",
+            "pg-lsclusters",
+            "syncobj-admin",
+        ]
+
+        for app, data in snapcraft["apps"].items():
+            if not bool(data.get("daemon")) and app not in skip:
+                subprocess.run(
+                    f"{snapcraft['name']}.{app} {override.get(app, '--help')}".split(),
+                    check=True,
+                )
+
+
+@pytest.mark.run(after="test_install")
+def test_all_services():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        skip = [
+            "pgbackrest-service",
+        ]
+
+        for app, data in snapcraft["apps"].items():
+            if bool(data.get("daemon")) and app not in skip:
+                subprocess.run(
+                    f"sudo snap start {snapcraft['name']}.{app}".split(), check=True
+                )
+                time.sleep(5)
+                service = subprocess.run(
+                    f"snap services {snapcraft['name']}.{app}".split(),
+                    check=True,
+                    capture_output=True,
+                )
+                subprocess.run(f"sudo snap stop {snapcraft['name']}.{app}".split())
+
+                assert "active" in str(service.stdout)
