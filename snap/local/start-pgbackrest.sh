d53d9c2aa9db0e2903b89dbb1d234abc075148b7
Dragomir Penev <dragomir.penev@canonical.com>
2023-04-06 17:26:16 +0300

    Create dirs in the snap

--- snap/local/start-pgbackrest.sh
+++ snap/local/start-pgbackrest.sh
@@ -2,5 +2,5 @@
 
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/pgbackrest server --config=$SNAP_DATA/etc/pgbackrest.conf "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/pgbackrest server --config=$SNAP_DATA/etc/pgbackrest/pgbackrest.conf "$@"
 
