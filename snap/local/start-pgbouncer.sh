ab6930cb38935294fbf1a52dbc5db02705506386
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2023-05-19 17:33:29 +0300

    [DPE-1648] Snap changes for pgbouncer charm (#10)
    * Create dirs for pgb
    
    * Add app for pgb charm service
    
    * Add config dir for pgb
    
    * Add network plugs

---
+++ snap/local/start-pgbouncer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# For security measures, daemons should not be run as sudo. Execute pgbouncer as the non-sudo user: snap_daemon.
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/sbin/pgbouncer "$@"
