950523a1f55b8eccf6d848cdff742e3a0a70ff5f
Dragomir Penev <dragomir.penev@canonical.com>
2023-05-25 21:18:06 +0300

    Exec setprivs for pgbouncer

--- snap/local/start-pgbouncer.sh
+++ snap/local/start-pgbouncer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # For security measures, daemons should not be run as sudo. Execute pgbouncer as the non-sudo user: snap_daemon.
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/sbin/pgbouncer "$@"
