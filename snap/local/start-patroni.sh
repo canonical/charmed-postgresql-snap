1d25e9643e3a471ad3ddeecdd8b40d71284fd71a
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-10-09 10:19:56 -0300

    Add locales

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # For security measures, daemons should not be run as sudo. Execute patroni as the non-sudo user: snap_daemon.
+export LOCPATH="${SNAP}"/usr/lib/locale
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/patroni $SNAP_DATA/etc/patroni/patroni.yaml "$@"
 
