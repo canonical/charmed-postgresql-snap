fb5d33721cbba289c6c693ef5b53eca2e29ec0e7
Lucas Gameiro Borges <lucas.borges@canonical.com>
2024-12-03 11:10:41 +0000

    try reposition tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -15,6 +15,40 @@ def test_install():
 
 
 @pytest.mark.run(after="test_install")
+def test_all_services():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        skip = [
+            "pgbackrest-service",
+        ]
+
+        for app, data in snapcraft["apps"].items():
+            if bool(data.get("daemon")) and app not in skip:
+                subprocess.run(
+                    f"sudo snap start {snapcraft['name']}.{app}".split(), check=True
+                )
+                time.sleep(5)
+                service = subprocess.run(
+                    f"snap services {snapcraft['name']}.{app}".split(),
+                    check=True,
+                    capture_output=True,
+                )
+                subprocess.run(f"sudo snap stop {snapcraft['name']}.{app}".split())
+
+                assert "active" in str(service.stdout)
+
+
+@pytest.mark.run(after="test_install")
+def test_version():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+        snap_version = snapcraft['version']
+        app_version = subprocess.check_output([f"{snapcraft['name']}.pg-isready", "--version"]).decode().split(" ")[2]
+        assert snap_version == app_version
+
+
+@pytest.mark.run(after="test_install")
 def test_all_apps():
     with open("snap/snapcraft.yaml") as file:
         snapcraft = yaml.safe_load(file)
@@ -52,37 +86,3 @@ def test_all_apps():
                     f"{snapcraft['name']}.{app} {override.get(app, '--help')}".split(),
                     check=True,
                 )
-
-
-@pytest.mark.run(after="test_install")
-def test_all_services():
-    with open("snap/snapcraft.yaml") as file:
-        snapcraft = yaml.safe_load(file)
-
-        skip = [
-            "pgbackrest-service",
-        ]
-
-        for app, data in snapcraft["apps"].items():
-            if bool(data.get("daemon")) and app not in skip:
-                subprocess.run(
-                    f"sudo snap start {snapcraft['name']}.{app}".split(), check=True
-                )
-                time.sleep(5)
-                service = subprocess.run(
-                    f"snap services {snapcraft['name']}.{app}".split(),
-                    check=True,
-                    capture_output=True,
-                )
-                subprocess.run(f"sudo snap stop {snapcraft['name']}.{app}".split())
-
-                assert "active" in str(service.stdout)
-
-
-@pytest.mark.run(after="test_install")
-def test_version():
-    with open("snap/snapcraft.yaml") as file:
-        snapcraft = yaml.safe_load(file)
-        snap_version = snapcraft['version']
-        app_version = subprocess.check_output([f"{snapcraft['name']}.pg-isready", "--version"]).decode().split(" ")[2]
-        assert snap_version == app_version
