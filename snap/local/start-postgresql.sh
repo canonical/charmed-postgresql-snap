a2c5b94acde1251ef388c1805d1e9c098f2b9264
Lucas Gameiro Borges <lucas.borges@canonical.com>
2025-01-23 14:29:54 +0000

    adapt mkdir and usermod commands

--- snap/local/start-postgresql.sh
+++ snap/local/start-postgresql.sh
@@ -4,10 +4,6 @@
 export LOCPATH="${SNAP}"/usr/lib/locale
 export PGDATA=$SNAP_DATA/pgsql/data
 
-mkdir /home/snap_daemon
-chown -R 584788:root /home/snap_daemon
-usermod -d /home/snap_daemon snap_daemon
-
 echo "Starting PostgreSQL database..."
 "${SNAP}/usr/bin/setpriv" --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}/usr/lib/postgresql/14/bin/postgres" -k /tmp -D "${PGDATA}"
 
