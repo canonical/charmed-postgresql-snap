e11ca8c9fa81ec941e2f2b165453b2cf6831d795
Lucas Gameiro Borges <lucas.borges@canonical.com>
2025-02-12 18:53:51 -0300

    use snap_common instead of snap_data

--- snap/local/start-postgresql.sh
+++ snap/local/start-postgresql.sh
@@ -2,6 +2,6 @@
 
 # For security measures, applications should not be run as sudo.
 export LOCPATH="${SNAP}"/usr/lib/locale
-export PGDATA=$SNAP_DATA/pgsql/data
+export PGDATA=$SNAP_COMMON/pgsql/data
 
 "${SNAP}/usr/bin/setpriv" --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}/usr/lib/postgresql/14/bin/postgres" -k /tmp -D "${PGDATA}"
