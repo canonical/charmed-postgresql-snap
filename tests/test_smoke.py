eeef78e303cf3a84bdbac7922048f0f43ccc8cb3
Dragomir Penev <dragomir.penev@canonical.com>
2025-07-09 15:54:48 +0300

    Add back pg_ctl

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -30,16 +30,11 @@ def test_all_apps():
             "patroni-raft-controller",
             "pg-buildext",
             "pg-conftool",
-            "pg-createcluster",
-            "pg-dropcluster",
-            "pg-upgradecluster",
             "pg-backupcluster",
             "pg-restorecluster",
             "pg-virtualenv",
             "pg-ctlcluster",
-            "pg-renamecluster",
             "pg-updatedicts",
-            "pg-lsclusters",
             "pgbouncer-server",
             "prometheus-pgbouncer-exporter",
         ]
@@ -92,7 +87,9 @@ def test_version():
         snapcraft = yaml.safe_load(file)
         snap_version = snapcraft["version"]
         app_version = (
-            subprocess.check_output([f"{snapcraft['name']}.pg-isready", "--version"])
+            subprocess.check_output(
+                [f"sudo {snapcraft['name']}.pg-isready", "--version"]
+            )
             .decode()
             .split(" ")[2]
         )
