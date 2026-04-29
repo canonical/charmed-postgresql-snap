e3481996e089becc95598b030c057c76627c6aaa
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2026-04-29 16:38:02 +0200

    improve layout

--- snap/local/start-pgbackrest-exporter.sh
+++ snap/local/start-pgbackrest-exporter.sh
@@ -9,7 +9,8 @@ else
     exec "${SNAP}/usr/bin/setpriv" \
         --clear-groups \
         --reuid _daemon_ \
-        --regid _daemon_ -- \
+        --regid _daemon_ \
+        -- \
         "${SNAP}/usr/bin/pgbackrest_exporter" \
         --backrest.config="/var/snap/charmed-postgresql/current/etc/pgbackrest/pgbackrest.conf" "$@"
 fi
